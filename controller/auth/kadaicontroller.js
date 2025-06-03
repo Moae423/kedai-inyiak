370b873a88591323a835037f4ce63d2eb9217c8c
daffa <hanura423@gmail.com>
2025-06-03 11:11:35 +0700

    update and delete function work and tested in API Client suuccess

--- controller/auth/kadaicontroller.js
+++ controller/auth/kadaicontroller.js
@@ -47,3 +47,34 @@ export const createDataBarang = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const updateDataBarang = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { namaBarang, jenisBarang, jmlBarang, tglMasuk, tglExpired, harga } =
+    req.body;
+  try {
+    const barang = await prisma.barang.update({
+      where: {
+        id,
+      },
+      data: { namaBarang, jenisBarang, jmlBarang, tglMasuk, tglExpired, harga },
+    });
+    res.status(200).json(barang, { message: "Data barang berhasil diupdate" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteDataBarang = async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const barang = await prisma.barang.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json(barang, { message: "Data barang berhasil dihapus" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- routes/barang.js
+++ routes/barang.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
   createDataBarang,
+  deleteDataBarang,
   getAllDataBarang,
   getSpesificBarang,
+  updateDataBarang,
 } from "../controller/auth/kadaicontroller.js";
 
 const router = express.Router();
@@ -11,4 +13,6 @@ const router = express.Router();
 router.get("/barang", getAllDataBarang);
 router.get("/barang/:id", getSpesificBarang);
 router.post("/barang", createDataBarang);
+router.put("/barang/:id", updateDataBarang);
+router.delete("/barang/:id", deleteDataBarang);
 export default router;
